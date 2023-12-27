b40a2be9c3861de7cdf916eac0fbbf1c4180ce69
Alban FALCOZ <alban.falcoz@orange.fr>
2023-12-27 22:38:54 +0100

    Création de pages jpg couleur inversées

--- dark_mode_pdf.py
+++ dark_mode_pdf.py
@@ -5,6 +5,22 @@ Created on Wed Dec 27 19:38:03 2023
 @author: alban
 """
 
+
+
+# import module
+from pdf2image import convert_from_path
+ 
+ 
+# Store Pdf with convert_from_path function
+images = convert_from_path('UNE ÉTUDE EN ROUGE.pdf',poppler_path=r"C:\Users\alban\OneDrive\Documents\Informatique\Python\library\poppler-23.11.0\Library\bin")
+ 
+#for i in range(len(images)):
+    #print(i)
+      # Save pages as images in the pdf
+    #images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+
+
+
 from PIL import Image
 from pdf2image import convert_from_path
 from random import *
@@ -13,7 +29,7 @@ imagePath = 'cat.jpg'
 #pil_image_lst = convert_from_path(imagePath,poppler_path=r'C:\Users\alban\OneDrive\Documents\Informatique\Python\library\poppler-23.11.0\Library\bin') # This returns a list even for a 1 page pdf
 #pil_image = pil_image_lst[0]
 newImagePath = 'darkmode.jpg'
-im = Image.open(imagePath)
+#im = Image.open(images)
 #im = Image.open(pil_image)
 
 
@@ -25,13 +41,13 @@ def redOrBlack (im):
     redcolor = (255,0,0)
     blackcolor = (0,0,0)
     for color in im.getdata():
-        if color >= (255,0,0)  :
-            newimdata.append( redcolor )
+        #if color >= (255,0,0)  :
+            #newimdata.append( redcolor )
             #print(newimdata[0],newimdata[1],newimdata[2])
             #comptRedPixel = comptRedPixel+1
             #print(comptRedPixel)
-        else:
-            newimdata.append( ((255-color[0],255-color[0],255-color[0])) )
+        #else:
+        newimdata.append( ((255-color[0],255-color[0],255-color[0])) )
             #newimdata.append( (randint(0,255),randint(0,255),randint(0,255)) )
         #comptPixel = comptPixel +1
         #print(comptPixel)
@@ -39,4 +55,8 @@ def redOrBlack (im):
     newim.putdata(newimdata)
     return newim
 
-redOrBlack(im).save(newImagePath)
+for i in range(len(images)):
+    print("Page numéro : " + str(i))
+      # Save pages as images in the pdf
+    #images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+    redOrBlack(images[i]).save('page'+ str(i) +'.jpg', 'JPEG')
